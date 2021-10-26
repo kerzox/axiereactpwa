7e555ff857c7c45565989885d89ad43bd148cb7e
Chris <chrisleotta1@gmail.com>
2021-10-27 09:48:33 +1000

    Added CDB.js for axios, .env for cloudant, a .js

---
+++ services/CDB.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { encode } from 'js-base64'
+
+//Using environmental variables embedded in React build
+const cloudantDBURL = process.env.REACT_APP_CLOUDANT_URL
+const username = process.env.REACT_APP_CLOUDANT_USERNAME
+const password = process.env.REACT_APP_CLOUDANT_PASSWORD
+
+//Base64 Encoded username password creates authkey
+
+const authKeyEncode = encode(`${username}:${password}`)
+
+export default axios.create({baseURL: `${cloudantDBURL}`,
+headers: {
+    'Authorization': `Basic ${authKeyEncode}`
+    }
+})
