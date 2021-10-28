ba30fae3357cf63c1e7d0432f4ebd79ba9a4492d
Chris <chrisleotta1@gmail.com>
2021-10-28 15:21:09 +1000

    another test

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class Home extends Component {
+
+
+
+    render(){
+		return(
+			<div>
+				<h1>Another page</h1>
+				<button type="button">Change value</button>
+			</div>
+		);
+	}
+
+
+
+
+}
+
+
+export default Home;
