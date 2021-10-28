5e304afa91438dfa595f06e9323d0bf50b54fd6d
Chris <chrisleotta1@gmail.com>
2021-10-29 08:37:27 +1000

    testing routing

--- src/App.js
+++ src/App.js
@@ -3,31 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import React, { Component } from 'react';
+import CDB from '../services/CDB';
+import { LineChart, Line } from 'recharts';
+import { Link } from "react-router-dom";
+
+
+// Pages
+
+import Home from "/Home";
+
+
 function App() {
+  
   
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> gooba gooba updated.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello this is a test
-        </a>
-        <ul>
-            <li><Link to="/Home">React</Link></li>
-        </ul>
-      
+    <Router id="image_background">
+    <div className="App" id="image_background" >
 
-      </header>
-    </div>
+        
+        <ul>
+            <li id="nav_logo"><img src="https://library.kissclipart.com/20180918/hkw/kissclipart-growth-transparent-background-icon-clipart-compute-bc96952ffedccf89.png" width="80" height="40" alt="Logo"></img></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Home">Stocks</Link></li>
+            <li style={{ float: "right" }}><Link to="/Home">Login</Link></li>
+            <li style={{ float: "right" }}><Link to="/Home">Register</Link></li>
+        </ul>
+        <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/Stocks"><Stocks /></Route>
+            <Route path="/Home"><Quote /></Route>
+            <Route path="/Home"><Price_History /></Route>
+            <Route path="/Home"><Register /></Route>
+            <Route path="/Home"><Login /></Route>
+           
+        </Switch>
+            </div>
+
+
+</Router>
   );
 }
 
