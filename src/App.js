3f828f825584c72cb35a3fc907f97db04bb4639b
Chris <chrisleotta1@gmail.com>
2021-10-29 08:49:56 +1000

    fixing routing again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import CDB from '../services/CDB';
+
 import { LineChart, Line } from 'recharts';
 
 
