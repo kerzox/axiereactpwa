b27103a9c9e271261a3e69acfc9b96159f11f867
Chris <chrisleotta1@gmail.com>
2021-10-29 10:29:16 +1000

    more tests

--- src/Heat_map.js
+++ src/Heat_map.js
@@ -13,4 +13,57 @@ export default class Heat_map extends Component {
 
     }
 
+    
+// this function runs each time the page is loaded, runs the function below
+componentDidMount(){
+    this.getAllDocuments();
+}
+
+// retrieves items from the database
+getAllDocuments(){
+
+ // this will retrieve the headers of the contents, not the contents for each header
+    CDB.get(`/a2_data/_all_docs`, {
+        responseType: 'json',
+    })
+
+        .then(response => {
+            const a2_datas = response.data.rows;
+            this.setState({ a2_datas })
+
+         })
+        .catch(error => console.error(`Error: ${error}`))
+}
+
+getOneDoc(docid){
+
+// this will retrieve the headers of the contents, not the contents for each header
+    CDB.get(`/a2_data/${docid}`, {
+          responseType: 'json',
+    })
+
+        .then(response => {
+            const a2_data = response.data;
+            this.setState({ a2_data })
+            console.log(a2_data)
+
+        })
+        .catch(error => console.error(`Error: ${error}`))
+
+
+    const d = Object.entries(this.state.a2_data).map(([key, value]) => (
+        <option key={key}>{key} - {value}</option>
+    ));
+
+
+    return (<div>{d}</div>)
+
+
+}
+
+
+
+
+
+
 }
