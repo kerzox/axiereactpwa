01b481ea941e2a23953f8d342bfc25cde5125961
Chris <chrisleotta1@gmail.com>
2021-10-29 10:28:00 +1000

    test again

--- src/Heat_map.js
+++ src/Heat_map.js
@@ -6,4 +6,11 @@ import { LineChart, Line } from 'recharts';
 export default class Heat_map extends Component {
 
     
+    state = {
+        a2_data: [],
+        a2_datas: [], 
+        selectedOption: null
+
+    }
+
 }
