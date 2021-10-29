2abd0fe6563dc5ee09d33a1fa498b630c9dd622b
Chris <chrisleotta1@gmail.com>
2021-10-29 15:02:44 +1000

    dwada

--- src/Heat_map.js
+++ src/Heat_map.js
@@ -33,7 +33,7 @@ export default class Heat_map extends Component {
             .then(response => {
                 const a2_datas = response.data.rows;
                 this.setState({ a2_datas })
-                console.log(a2_datas[0])
+                console.log(a2_datas[0].id)
              })
             .catch(error => console.error(`Error: ${error}`))
     }
