74ec0feeb7ce80c00babdb2ccd427110a39418b8
Chris <chrisleotta1@gmail.com>
2021-10-29 11:38:33 +1000

    awdad

--- src/CDB.js
+++ src/CDB.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { encode } from 'js-base64';
 
 //Using environmental variables embedded in React build
-const cloudantDBURL = 'https://c6b339ee-b547-4f06-a6f7-cf0c9abec3b2-bluemix.cloudantnosqldb.appdomain.cloud'
-const username = 'apikey-v2-zn38fvzio9k56aa20mzk2xxik8qa048d9fxkg1dyax6'
-const password = '6bee85a97fb4b46343a685876474a03a'
+const cloudantDBURL = process.env.REACT_APP_CLOUDANT_URL
+const username = process.env.REACT_APP_CLOUDANT_USERNAME
+const password = process.env.REACT_APP_CLOUDANT_PASSWORD
 
 //Base64 Encoded username password creates authkey
 
