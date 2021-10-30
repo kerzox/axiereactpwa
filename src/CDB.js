d467d865ded25fcfdfc48a0e9432fc6de402aae3
Chris <chrisleotta1@gmail.com>
2021-10-30 22:59:36 +1000

    dwa

--- src/CDB.js
+++ src/CDB.js
@@ -16,7 +16,7 @@ export default axios.create({
     baseURL: `${cloudantDBURL}`,
     headers: {
         'Authorization': `Basic ${authKeyEncode}`,
-         //"Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "*",
          //"Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
          //"Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
        
