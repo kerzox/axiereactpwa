5e9510587eeb1d2a7f981142f512fa6459221267
Chris <chrisleotta1@gmail.com>
2021-10-31 12:51:52 +1000

    dwada

--- src/CDB.js
+++ src/CDB.js
@@ -15,8 +15,8 @@ const authKeyEncode = encode(`${username}:${password}`)
 export default axios.create({
     baseURL: `${cloudantDBURL}`,
     headers: {
-        'Authorization': `Basic ${authKeyEncode}`
-        //"Access-Control-Allow-Origin": "*",
+        'Authorization': `Basic ${authKeyEncode}`,
+        "Access-Control-Allow-Origin": "*",
          //"Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
          //"Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
        
@@ -28,4 +28,3 @@ export default axios.create({
 
 });
 
-//"*":`${cors()}`
