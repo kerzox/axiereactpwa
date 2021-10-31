bccb46b4d1b5edf970a97f4df7c1ade0596eb0b5
Chris <chrisleotta1@gmail.com>
2021-10-31 13:57:00 +1000

    dwad

--- src/Heat_map.js
+++ src/Heat_map.js
@@ -77,7 +77,7 @@ export default class Heat_map extends Component {
         console.log("getting all docs now ")
      // this will retrieve the headers of the contents, not the contents for each header
         CDB.get(`https://c6b339ee-b547-4f06-a6f7-cf0c9abec3b2-bluemix.cloudantnosqldb.appdomain.cloud/a2_data/_all_docs`, {
-            responseType: 'json',
+            responseType: 'json'
         })
 
             .then(response => {
